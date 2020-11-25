e7eb94d275cb6ada653e8e387bbe2939ad4eaf39
Ubuntu <ubuntu@ip-172-31-53-99.ec2.internal>
2020-11-25 02:31:36 +0000

    Add detail book

--- app/controllers/books_controller.rb
+++ app/controllers/books_controller.rb
@@ -71,6 +71,6 @@ class BooksController < ApplicationController
 
     # Only allow a list of trusted parameters through.
     def book_params
-      params.require(:book).permit(:title, :number̲of̲pages, :image)
+      params.require(:book).permit(:title, :number̲of̲pages, :detail, :image)
     end
 end
